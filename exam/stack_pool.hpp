523dd73a123c2b68029389d57b275f11632f6813
lorenzo <lorenztrubian@gmail.com>
2022-02-11 22:14:58 +0100

    work in progress

--- exam/stack_pool.hpp
+++ exam/stack_pool.hpp
@@ -14,27 +14,27 @@ class stack_pool{
   const node_t& node(stack_type x) const noexcept { return pool[x-1]; }
 
   public:
-  stack_pool();
+  stack_pool() : free_nodes{0} {} ;
   explicit stack_pool(size_type n) { pool.reserve(n); }; // reserve n nodes in the pool
     
-  using iterator = stack_type;
-  using const_iterator = const stack_type;
+  using iterator = stack_type*;
+  using const_iterator = const stack_type*;
 
-  iterator begin(stack_type x) noexcept {return (x-1);} ;
+  iterator begin(stack_type x) noexcept {return (&x);} ;
   iterator end(stack_type ) noexcept { return stack_type(0); }; // this is not a typo
     
-  const_iterator begin(stack_type x) const noexcept {return (x-1);};
+  const_iterator begin(stack_type x) const noexcept {return (&x);};
   const_iterator end(stack_type ) const noexcept { return stack_type(0); }; 
   
-  const_iterator cbegin(stack_type x) const noexcept {return (x-1);};
+  const_iterator cbegin(stack_type x) const noexcept {return (&x);};
   const_iterator cend(stack_type ) const noexcept { return stack_type(0); };
     
-  stack_type new_stack(); // return an empty stack
+  stack_type new_stack() {return stack_type(0);}; // return an empty stack
 
   void reserve(size_type n) {pool.reserve(n); }; // reserve n nodes in the pool
-  size_type capacity() const { return pool.capacity(); }; // the capacity of the pool
+  size_type capacity() const noexcept{ return pool.capacity(); }; // the capacity of the pool
 
-  bool empty(stack_type x) const;
+  bool empty(stack_type x) const  {return ( x > 0 ) ? false : true ;} ;
 
   stack_type end() const noexcept { return stack_type(0); }
 
@@ -44,10 +44,54 @@ class stack_pool{
   stack_type& next(stack_type x) noexcept { return node(x).next; };
   const stack_type& next(stack_type x) const noexcept{ return node(x).next; };
 
-  stack_type push(const T& val, stack_type head) ;
-  stack_type push(T&& val, stack_type head);
-  
-  stack_type pop(stack_type x); // delete first node
+  stack_type push(const T& val, stack_type head) {
+    if (empty(free_nodes)){
+      pool[0].value = val;
+      pool[0].next = head;
+      ++free_nodes;
+      pool[free_nodes].next = pool.end();
+      return 1;
+    }
+    else{
+      stack_type new_head{free_nodes};
+      if ( pool[free_nodes].next == stack_type(0)){
+        ++free_nodes;
+        pool[free_nodes].next = stack_type(0);        
+      }
+      else{
+        free_nodes = pool[free_nodes].next;
+      }
+      pool[new_head].value = val;
+      pool[new_head].next = head;
+      return new_head;
+    }
+
+  } ;
+  stack_type push(T&& val, stack_type head){
+    if (empty(free_nodes)){
+      pool[0].value = val;
+      pool[0].next = head;
+      ++free_nodes;
+      pool[free_nodes].next = stack_type(0);
+      return 1;
+    }
+    else{
+      stack_type new_head{free_nodes};
+      if ( pool[free_nodes].next == stack_type(0)){
+        ++free_nodes;
+        pool[free_nodes].next = stack_type(0);        
+      }
+      else{
+        free_nodes = pool[free_nodes].next;
+      }
+      pool[new_head].value = val;
+      pool[new_head].next = head;
+      return new_head;
+    }
+  };
+  stack_type pop(stack_type x){
+    node(x).next = 
+  }; // delete first node
 
   stack_type free_stack(stack_type x); // free entire stack
 };
